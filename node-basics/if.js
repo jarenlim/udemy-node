c95f3b49040466303da0252dbcab0865b67f3c0c
jarenlim <jarenlim@gmail.com>
2019-02-09 12:21:41 +0800

    If conditionals

---
+++ node-basics/if.js
@@ -0,0 +1,10 @@
+var averageGrade = 100;
+var name = 'John';
+
+if (averageGrade === 100 || name === 'Jaren') {
+    console.log('You got a perfect grade!');
+} else if (averageGrade >= 70) {
+    console.log('You passed the class! Way to go!');
+} else {
+    console.log('You failed the course. Sorry!');
+}
