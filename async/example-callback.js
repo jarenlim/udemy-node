903be5530322ec35353f5f451766f146ef17d49d
jarenlim <jarenlim@gmail.com>
2019-02-12 18:22:33 +0800

    Weather App 1

--- async/example-callback.js
+++
@@ -1,7 +0,0 @@
-// var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial';
-// var url = 'http://api.openweathermap.org/data/2.5/weather?appid=PASTE_API_KEY_HERE&q=' + encodedLocation + '&units=imperial';
-var weather = require('./weather.js');
-
-weather(function (currentWeather) {
-    console.log(currentWeather);
-});
